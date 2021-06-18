f3b0dbc97fe138c58a967d7e199f81c67d48ed28
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-18 20:22:45 +0530

    update 🚀:App.js
    - Imported newly created Roomcards component

--- clubhouse-roomfeed/src/App.js
+++ clubhouse-roomfeed/src/App.js
@@ -1,10 +1,12 @@
 import Nav from "./Components/Nav";
 import StickyFooter from "./Components/StickyFooter";
+import RoomCards from "./Components/RoomCards";
 function App() {
 	return (
 		<div className="App">
 			<Nav />
 			<StickyFooter />
+			<RoomCards />
 		</div>
 	);
 }
