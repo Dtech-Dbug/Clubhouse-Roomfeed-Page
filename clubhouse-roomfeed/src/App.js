4a9e60c4b6d75d82b3d6304384c3142d60bc0561
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-18 23:29:29 +0530

    update 🚀 : RoomFeed.js

--- clubhouse-roomfeed/src/App.js
+++ clubhouse-roomfeed/src/App.js
@@ -1,12 +1,13 @@
 import Nav from "./Components/Nav";
 import StickyFooter from "./Components/StickyFooter";
-import RoomCards from "./Components/RoomCards";
+import RoomFeed from "./Components/RoomFeed";
+
 function App() {
 	return (
 		<div className="App">
 			<Nav />
 			<StickyFooter />
-			<RoomCards />
+			<RoomFeed />
 		</div>
 	);
 }
