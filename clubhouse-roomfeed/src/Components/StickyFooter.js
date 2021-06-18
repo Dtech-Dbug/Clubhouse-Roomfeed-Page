b6c42ee237b69336a8345fef771fec574fd11a69
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-18 19:42:10 +0530

    docs && stylings 🚀: StickyFoote.js component
    - created StickyFooter component'
    - updated stylings

---
+++ clubhouse-roomfeed/src/Components/StickyFooter.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const StickyFooter = () => {
+	return (
+		<div className="footer-wrapper">
+			<footer>
+				<button>+ Start Room</button>
+				<i class="fa fa-th"></i>
+			</footer>
+		</div>
+	);
+};
+
+export default StickyFooter;
