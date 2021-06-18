3f0bac3d66a1638d1aca98e60bfd31937e62e263
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-18 22:05:49 +0530

    update 🚀: StickyFooter.js
    - changed height of grid FA icon

--- clubhouse-roomfeed/src/Components/StickyFooter.js
+++ clubhouse-roomfeed/src/Components/StickyFooter.js
@@ -4,8 +4,8 @@ const StickyFooter = () => {
 	return (
 		<div className="footer-wrapper">
 			<footer>
-				<button>+ Start Room</button>
-				<i class="fa fa-th" style={{ fontSize: "1.5rem" }}></i>
+				<button>+ Start A Room</button>
+				<i class="fa fa-th" style={{ fontSize: "1.6rem" }}></i>
 			</footer>
 		</div>
 	);
