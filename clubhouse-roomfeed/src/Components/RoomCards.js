545968738578015932f35435fd68884224e31d84
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-18 21:47:32 +0530

    Updated docs and stylings 🎉: RoomCard.js

--- clubhouse-roomfeed/src/Components/RoomCards.js
+++ clubhouse-roomfeed/src/Components/RoomCards.js
@@ -1,14 +1,59 @@
 import React from "react";
+import User from "../Image/user.jpg";
+import { MessageOutlined } from "@ant-design/icons";
+import Comments from "../Image/comments.svg";
+import CommentsRegular from "../Image/commentsRegular.svg";
 
 const RoomCards = () => {
 	return (
 		<div className="room-cards">
 			<div className="room-card-header">
-				<h3>Test</h3>{" "}
-				<span>
-					<i class="fa fa-home"></i>
-				</span>
+				<h3>
+					Test <i style={{ color: "crimson" }} className="fa fa-home"></i>
+				</h3>{" "}
 				<h4>Test Test Test</h4>
+			</div>
+
+			<div className="room-card-content">
+				<div className="user-images">
+					<img className="user-images user-image-1" src={User} alt="Oops!" />
+					<img className="user-images user-image-2" src={User} alt="Oops!" />
+				</div>
+
+				<div className="room-users">
+					<div className="users">
+						<h4>User 1</h4>
+						<img
+							className="comment"
+							src={CommentsRegular}
+							style={{ backgroundClip: "contentBox" }}
+							alt=""
+						/>
+					</div>
+
+					<div className="users">
+						<h4>User 2</h4>
+						<img
+							className="comment"
+							src={CommentsRegular}
+							style={{ backgroundClip: "contentBox" }}
+							alt=""
+						/>
+					</div>
+					<div className="users">
+						<h4>User 2</h4>
+						<img
+							className="comment"
+							src={CommentsRegular}
+							style={{ backgroundClip: "contentBox" }}
+							alt=""
+						/>
+					</div>
+
+					<div className="room-info">
+						567 <i class="fa fa-user"></i> / 17 <MessageOutlined />
+					</div>
+				</div>
 			</div>
 		</div>
 	);
