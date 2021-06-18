fcad7cd7843f4171a363375d82a1641144547778
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-18 20:22:26 +0530

    updated 🚀: Roomcard.js => test with layout

---
+++ clubhouse-roomfeed/src/Components/RoomCards.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const RoomCards = () => {
+	return (
+		<div className="room-cards">
+			<div className="room-card-header">
+				<h3>Test</h3>{" "}
+				<span>
+					<i class="fa fa-home"></i>
+				</span>
+				<h4>Test Test Test</h4>
+			</div>
+		</div>
+	);
+};
+
+export default RoomCards;
+//<i class="far fa-comment-dots"></i>
