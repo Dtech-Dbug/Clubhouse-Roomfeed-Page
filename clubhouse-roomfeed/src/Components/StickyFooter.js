b11d703d24fc3b3d89cf8688abe3f52aadf72056
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-19 20:12:33 +0530

    Update ✨: StickyFooter.js
    - Conditional rendering of style of the fontawesome grid icon

--- clubhouse-roomfeed/src/Components/StickyFooter.js
+++ clubhouse-roomfeed/src/Components/StickyFooter.js
@@ -1,11 +1,17 @@
 import React from "react";
 
-const StickyFooter = () => {
+const StickyFooter = ({ theme, dark }) => {
 	return (
 		<div className="footer-wrapper">
 			<footer>
 				<button>+ Start A Room</button>
-				<i class="fa fa-th" style={{ fontSize: "1.6rem" }}></i>
+				<i
+					class="fa fa-th"
+					style={{
+						fontSize: "1.6rem",
+						color: theme === dark ? "white" : "black",
+					}}
+				></i>
 			</footer>
 		</div>
 	);
