592e032093f4b06b72f971e1187a03324ac60029
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-19 20:13:38 +0530

    update 🚀: Toggle.js

--- clubhouse-roomfeed/src/Components/Toggle.js
+++ clubhouse-roomfeed/src/Components/Toggle.js
@@ -3,25 +3,30 @@ import { Switch } from "antd";
 
 const Toggle = ({ theme, dark, light, changeTheme, color }) => {
 	return (
-		<div
-			className="Toggle-Container"
-			style={{
-				boxShadow: theme === light ? "0px 3px 6px black" : "0px 0px 2px white",
-			}}
-		>
-			<Switch
-				style={{ background: theme === dark ? "grey" : "yellow" }}
-				checked={theme === dark}
-				onChange={changeTheme}
-				checkedChildren="🕯"
-				unCheckedChildren="🔦"
-			/>
+		<div className="Toggle-Parent-Container">
+			<div
+				className="Toggle-Container"
+				style={{
+					boxShadow:
+						theme === light ? "0px 3px 6px black" : "0px 0px 2px white",
+				}}
+			>
+				<Switch
+					style={{
+						background: theme === dark ? "grey" : "yellow",
+					}}
+					checked={theme === dark}
+					onChange={changeTheme}
+					checkedChildren="🕯"
+					unCheckedChildren="🔦"
+				/>
 
-			{theme === dark ? (
-				<h3 style={{ color: color }}>👈 Lights In 💡 </h3>
-			) : (
-				<h3 style={{ color: color }}>👈 Lights Out 🏮 </h3>
-			)}
+				{theme === dark ? (
+					<h3 style={{ color: color }}>👈 Lights In 💡 </h3>
+				) : (
+					<h3 style={{ color: color }}>👈 Lights Out 🏮 </h3>
+				)}
+			</div>
 		</div>
 	);
 };
