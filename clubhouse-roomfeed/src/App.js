618a87e06a69fc984147e5ad0c668723db7a2010
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-19 14:21:55 +0530

    update 🚀: App.js => dark mode feat
    - Imported SWITCH from antd
    - introduced dark mode on switch
    - passed theme as props to app

--- clubhouse-roomfeed/src/App.js
+++ clubhouse-roomfeed/src/App.js
@@ -1,13 +1,32 @@
+import { useState, useEffect } from "react";
 import Nav from "./Components/Nav";
 import StickyFooter from "./Components/StickyFooter";
 import RoomFeed from "./Components/RoomFeed";
+import { Switch } from "antd";
 
 function App() {
+	const light = "#f2f0e4";
+	const dark = "black";
+	const [theme, setTheme] = useState(light);
+	useEffect(() => {
+		console.log("theme is :", theme);
+	});
+
+	function changeTheme(value) {
+		console.log(value);
+		setTheme(value ? dark : light);
+	}
 	return (
-		<div className="App">
-			<Nav />
+		<div className="App" style={{ background: theme, height: "150vh" }}>
+			<Nav theme={theme} />
+			<Switch
+				checked={theme === dark}
+				onChange={changeTheme}
+				checkedChildren="Dark"
+				unCheckedChildren="Light"
+			/>
 			<StickyFooter />
-			<RoomFeed />
+			<RoomFeed theme={theme} />
 		</div>
 	);
 }
